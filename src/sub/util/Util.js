e9aec149bf8dfaca4e9ee5be9559451d0503a7cc
t_fukushi <t_fukushi@where123.jp>
2018-10-03 13:44:28 +0900

    物画面作成

--- src/sub/util/Util.js
+++ src/sub/util/Util.js
@@ -23,7 +23,7 @@ export const colorCd4display = (str) => {
   return "#" + str.replace("#", "").slice(0, 8)
 }
 
-export const hasValue = (str) => str !== undefined && str.length != 0
+export const hasValue = (str) => str !== undefined && str !== null && str.length != 0
 export const detectEncoding = (str) => jschardet.detect(str)
 
 export const csv2Obj = (str) => {
