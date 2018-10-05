085025681d63f4120e4aae798cba6164e00502c5
tyadawh <t_yada@where123.jp>
2018-10-05 11:16:55 +0100

    利用数画面

--- src/pages/analyze/numUsers.vue
+++ src/pages/analyze/numUsers.vue
@@ -3,6 +3,15 @@
     <breadcrumb :items="items" :reload="true" />
     <div class="container">
       <p></p>
+      <b-row>
+        <b-form inline class="mt-2">
+          <label class="mr-2">{{ $t('label.analyzeMonth') }}</label>
+          <v-select v-model="analyzeMonth" :options="analyzeMonthOptions" required class="ml-2"></v-select>
+          <label class="mr-2">{{ $t('label.analyzeDay') }}</label>
+          <v-select :options="analyzeDayOptions" required class="ml-2"></v-select>
+          <b-button size="sm" variant="info" v-t="'label.search'" @click="search()"></b-button> 
+        </b-form>
+      </b-row>
       <b-row align-h="end">
         <b-col md="2" class="mb-3 mr-3">
           <b-button variant='outline-primary' @click="download()" v-t="'label.download'" />
@@ -57,6 +66,53 @@ export default {
     }
   },
   computed: {
+    analyzeMonthOptions() {
+      return [
+        {label: "2018/10", value: 201810 },
+        {label: "2018/09", value: 201809 },
+        {label: "2018/08", value: 201808 },
+        {label: "2018/07", value: 201807 },
+        {label: "2018/06", value: 201806 },
+        {label: "2018/05", value: 201805 },
+        {label: "2018/04", value: 201804 },
+      ]
+    },
+    analyzeDayOptions() {
+      return [
+        {label: " ", value: 0 },
+        {label: "1", value: 1 },
+        {label: "2", value: 2 },
+        {label: "3", value: 3 },
+        {label: "4", value: 4 },
+        {label: "5", value: 5 },
+        {label: "6", value: 6 },
+        {label: "7", value: 7 },
+        {label: "8", value: 8 },
+        {label: "9", value: 9 },
+        {label: "10", value: 10 },
+        {label: "11", value: 11 },
+        {label: "12", value: 12 },
+        {label: "13", value: 13 },
+        {label: "14", value: 14 },
+        {label: "15", value: 15 },
+        {label: "16", value: 16 },
+        {label: "17", value: 17 },
+        {label: "18", value: 18 },
+        {label: "19", value: 19 },
+        {label: "20", value: 20 },
+        {label: "21", value: 21 },
+        {label: "22", value: 22 },
+        {label: "23", value: 23 },
+        {label: "24", value: 24 },
+        {label: "25", value: 25 },
+        {label: "26", value: 26 },
+        {label: "27", value: 27 },
+        {label: "28", value: 28 },
+        {label: "29", value: 29 },
+        {label: "30", value: 30 },
+        {label: "31", value: 31 },
+      ]
+    },
     getTheme () {
       const theme = getTheme(this.$store.state.loginId)
       return 'outline-' + theme
@@ -77,7 +133,9 @@ export default {
   methods: {
     async fetchData(payload) {
       try {
-        let numUsers = await AppServiceHelper.fetchList2('numUsers', '/office/numUsers', 'numOfUsers')
+        let numUsers = await AppServiceHelper.fetchList2(
+          'numUsers', '/office/numUsers', 'numOfUsers'
+        )
         if (payload && payload.done) {
           payload.done()
         }
@@ -99,6 +157,15 @@ export default {
     ...mapMutations('monitor', [
       'replaceMonitor', 
     ]),
+    async search() {
+      let anaMonth = this.analyzeMonth.value
+      let numUsers = await AppServiceHelper.fetchList2(
+        'numUsers',
+        '/office/numUsers/' + anaMonth,
+        'numOfUsers'
+      )
+      this.replaceMonitor({numUsers})
+    },
   }
 }
 </script>
