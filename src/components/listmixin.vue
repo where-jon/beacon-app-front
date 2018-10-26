5605ca820218da2e3619cc89c29f671733ac0879
t_fukushi <t_fukushi@where123.jp>
2018-10-26 13:20:23 +0900

    カテゴリ・グループ一覧の形と表示色を表示にまとめる

--- src/components/listmixin.vue
+++ src/components/listmixin.vue
@@ -2,10 +2,21 @@
 <script>
 import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
 import reloadmixinVue from './reloadmixin.vue'
+import { SHAPE } from '../sub/constant/Constants'
+import * as Util from '../sub/util/Util'
 
 export default {
   mixins: [reloadmixinVue],
   methods: {
+    getStyleDisplay(entity){
+      return entity.map((val) => ({
+        "color": Util.colorCd4display(val.color),
+        "background-color": Util.colorCd4display(val.bgColor),
+        "text-align": "center",
+        border: `solid 3px ${Util.colorCd4display(val.bgColor)}`,
+        "border-radius": val.shape == SHAPE.CIRCLE? "50%": val.shape == SHAPE.ROUND_SQUARE? "10px": null,
+      }))
+    }
   }
 }
 </script>
