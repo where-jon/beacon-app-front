0c87f3f6c721ff2554da2a46d1485015d2a42b5a
t_fukushi <t_fukushi@where123.jp>
2018-11-01 15:39:26 +0900

    パスワード周り修正

--- src/components/editmixin.vue
+++ src/components/editmixin.vue
@@ -11,7 +11,6 @@ import { APP } from '../sub/constant/config.js'
 import * as HtmlUtil from '../sub/util/HtmlUtil'
 import * as Util from '../sub/util/Util'
 import commonmixinVue from './commonmixin.vue';
-import { UPDATE_ONLY_NN, ENCODE_PASS } from '../sub/constant/Constants'
 
 let that
 
@@ -257,7 +256,7 @@ export default {
         throw new Error(`${this.$i18n.tnl('message.csvSameKey')}${this.formatErrorLine(sameLine)}`)
       }
 
-      await AppServiceHelper.bulkSave(this.appServicePath, entities, UPDATE_ONLY_NN.NONE, ENCODE_PASS.FALSE)
+      await AppServiceHelper.bulkSave(this.appServicePath, entities)
       if(Util.isArray(mainCol)){
         let col = null
         mainCol.forEach((val, idx) => {
