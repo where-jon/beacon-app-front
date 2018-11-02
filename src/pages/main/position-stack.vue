fbccd7b2b4229151344fe22b22bc0e5df02401c4
a_tanno <a_tanno@where123.jp>
2018-11-02 09:37:02 +0900

    feature/406 スタイル適用、横並びまで

--- src/pages/main/position-stack.vue
+++ src/pages/main/position-stack.vue
@@ -3,7 +3,11 @@
     <breadcrumb :items="items" :extraNavSpec="extraNavSpec"
         :reload="reload" />
     <b-table :items="eachAreas" :fields="fields" thead-class="d-none">
-
+      <template slot="icons" slot-scope="row">
+          <div v-for="position in row.item.positions" :key="position.areaId"
+              class="d-inline-block mx-1"
+              v-bind:style="getStyleDisplay1(position.display)">A</div>
+      </template>
     </b-table>
   </div>
 </template>
@@ -13,10 +17,12 @@ import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
 import * as mock from '../../assets/mock/mock'
 import breadcrumb from '../../components/breadcrumb.vue'
 import commonmixinVue from '../../components/commonmixin.vue'
+import listmixinVue from '../../components/listmixin.vue'
 import * as EXCloudHelper from '../../sub/helper/EXCloudHelper'
 import * as StateHelper from '../../sub/helper/StateHelper'
 import * as PositionHelper from '../../sub/helper/PositionHelper'
 import { DISP, DEV } from '../../sub/constant/config'
+import { SHAPE } from '../../sub/constant/Constants'
 import * as Util from '../../sub/util/Util'
 
 let that
@@ -27,6 +33,7 @@ export default {
   },
   mixins: [
     commonmixinVue,
+    listmixinVue,
    ],
   data() {
     return {
@@ -37,6 +44,12 @@ export default {
         {key: 'icons'},
       ],
       eachAreas: [],
+      styles: [],
+      defaultDisplay: {
+        color: DISP.TX_COLOR,
+        bgColor: DISP.TX_BGCOLOR,
+        shape: SHAPE.CIRCLE,
+      },
       items: [
         {
           text: this.$i18n.t('label.main'),
@@ -93,13 +106,15 @@ export default {
         // positionデータ取得
         let positions = await EXCloudHelper.fetchPosition(this.exbs, this.txs)
 
+        // 移動平均数分のポジションデータを保持する
         let orgPositions = _.clone(this.orgPositions)
-        if (orgPositions.length >= DISP.MOVING_AVERAGE) { // 移動平均数分のポジションデータを保持する
+        if (orgPositions.length >= DISP.MOVING_AVERAGE) {
           orgPositions.shift()
         }
         orgPositions.push(positions)
         this.replaceMain({orgPositions})
 
+        // 在席表示と同じ、表示txを取得する。
         let now = !DEV.USE_MOCK_EXC ? new Date().getTime()
             : mock.positions_conf.start + this.count++ * mock.positions_conf.interval
         const correctPositions = PositionHelper.correctPosId(this.orgPositions, now)
@@ -108,10 +123,27 @@ export default {
           _.some(correctPositions, (cPos) => pos.btx_id == cPos.btx_id)
         )
 
-        let tempArea = _.map(this.areas, (area) => ({
+        // スタイルをセット
+        positions = _.map(positions, pos => {
+          // 設定により、カテゴリとグループのどちらの設定で表示するかが変わる。
+          let display
+          if (pos.tx) {
+            const styleSrc = pos.tx[DISP.DISPLAY_PRIORITY[0]] || pos.tx[DISP.DISPLAY_PRIORITY[1]]
+            display = styleSrc && styleSrc.display
+          }
+          display = display || this.defaultDisplay          
+          return {
+            ...pos,
+            display,
+          }
+        })
+
+        // エリアごとに分類
+        const tempArea = _.map(this.areas, (area) => ({
           areaId: area.areaId,
           label: area.areaName,
           positions: []}))
+
         _.forEach(positions, (pos) => {
           const posAreaId = Util.getValue(pos, 'exb.location.areaId', null)
           _.forEach(tempArea, (area) => {
