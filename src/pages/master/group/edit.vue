17bfaf4ebd3c6d6dfaa8e47cce625a1b17d19ca7
t_fukushi <t_fukushi@where123.jp>
2019-01-17 10:50:44 +0900

    "color","bgColor"をCSV形式でダウンロードした際は頭に"#"を付ける形式にする

--- src/pages/master/group/edit.vue
+++ src/pages/master/group/edit.vue
@@ -117,9 +117,9 @@ export default {
         ruby: this.form.ruby,
         description: this.form.description,
         display: {
-          shape: this.form.displayShape,
-          color: Util.colorCd4db(this.form.displayColor),
-          bgColor: Util.colorCd4db(this.form.displayBgColor),
+          shape: `${this.form.displayShape}`,
+          color: Util.colorCd4display(this.form.displayColor),
+          bgColor: Util.colorCd4display(this.form.displayBgColor),
         },
       }
       return await AppServiceHelper.bulkSave(this.appServicePath, [entity])
