7083a4bfc6abc235c072889e7f45956ea9fcebea
p_jung <p_jung@where123.jp>
2019-01-18 11:22:28 +0900

    formを変更

--- src/pages/master/mailTemplate/edit.vue
+++ src/pages/master/mailTemplate/edit.vue
@@ -25,31 +25,31 @@
         <!--通知媒体-->
         <b-form-group v-show="notify.length > 1">
           <label v-t="'label.notifyMedium'" />
-          <b-form-radio-group v-model="selected" :options="notify" :disabled="!isEditable" @change="radioChange" />
+          <b-form-radio-group v-model="form.notifyMedium" :options="notify" :disabled="!isEditable" @change="radioChange" />
         </b-form-group>
 
         <!--通知先-->
         <b-form-group v-if="bNotifyTo">
           <label v-t="'label.notifyTo'" />
-          <b-form-textarea v-model="form.notifyTo" :rows="3" :max-rows="6" :readonly="!isEditable" maxlength="1000" />
+          <b-form-textarea v-model="form.notifyTo" :rows="3" :max-rows="6" maxlength="2000" />
         </b-form-group>
 
         <!--件名-->
         <b-form-group v-if="bSubject">
           <label v-t="'label.subject'" />
-          <input v-model="form.subject" :readonly="!isEditable" type="text" maxlength="20" class="form-control" required>
+          <input v-model="form.subject" type="text" maxlength="255" class="form-control" required>
         </b-form-group>
 
         <!--From-->
         <b-form-group>
           <label v-t="'label.mailFrom'" />
-          <input v-model="form.mailFrom" :readonly="!isEditable" type="text" maxlength="20" class="form-control" required>
+          <input v-model="form.mailFrom" type="text" maxlength="255" class="form-control" required>
         </b-form-group>
 
         <!--テンプレート-->
         <b-form-group>
           <label v-t="'label.template'" />
-          <b-form-textarea v-model="form.template" :rows="3" :max-rows="6" :readonly="!isEditable" maxlength="1000" />
+          <b-form-textarea v-model="form.template" :rows="3" :max-rows="6"/>
         </b-form-group>
 
         <b-button v-t="'label.back'" type="button" variant="outline-danger" class="mr-2 my-1" @click="backToList" />
@@ -64,7 +64,6 @@
 
 <script>
 import { mapState } from 'vuex'
-import * as StateHelper from '../../../sub/helper/StateHelper'
 import * as ViewHelper from '../../../sub/helper/ViewHelper'
 import * as AppServiceHelper from '../../../sub/helper/AppServiceHelper'
 import editmixinVue from '../../../components/mixin/editmixin.vue'
@@ -72,7 +71,7 @@ import * as Util from '../../../sub/util/Util'
 import breadcrumb from '../../../components/layout/breadcrumb.vue'
 import alert from '../../../components/parts/alert.vue'
 import { getButtonTheme } from '../../../sub/helper/ThemeHelper'
-import { CATEGORY, ZONE, NOTIFY_STATE,NOTIFY_MIDIUM } from '../../../sub/constant/Constants'
+import { NOTIFY_STATE,NOTIFY_MIDIUM } from '../../../sub/constant/Constants'
 import showmapmixin from '../../../components/mixin/showmapmixin.vue'
 import { APP } from '../../../sub/constant/config.js'
 
@@ -88,14 +87,12 @@ export default {
       id: 'notifyTemplateId',
       notify: _.slice(NOTIFY_MIDIUM.getTypes(), 0, 2).filter((val) => APP.NOTIFY_MIDIUM_TYPES.includes(val.value)),
       backPath: '/master/mailTemplate',
-      appServicePath: '/core/rcvexcloud/template/save',
-      selected: 0,
+      appServicePath: '/core/rcvexcloud',
       form: ViewHelper.extract(this.$store.state.app_service.template,
         ['notifyTemplateId', 'notifyTemplateKey' , 'notifyMedium', 'notifyTo', 'subject', 'mailFrom', 'template' ]),
       areaNames: [],
       categoryNames: [],
       isEnableNameText: true,
-      zones: [],
       isRegist: false,
       bNotifyTo:true,
       bSubject:true,
@@ -127,12 +124,10 @@ export default {
       return 'outline-' + theme
     },
     ...mapState('app_service', [
-      'zone',
+      'template',
     ]),
   },
   created() {
-    this.initAreaNames()
-    this.initCategoryNames()
   },
   methods: {
     reset () {
@@ -140,7 +135,7 @@ export default {
     async signalChange(evt) {
       if (evt == 'TX_DELIVERY_NOTIFY') {
         this.bNotifyTo = false
-        this.selected =1
+        this.form.notifyMedium = 1
       }else{
         this.bNotifyTo = true
       }
@@ -154,25 +149,14 @@ export default {
       }
       console.log(evt)
     },
-    async initAreaNames() {
-      await StateHelper.load('area')
-      this.areaNames = StateHelper.getOptionsFromState('area', false, true)
-      console.log(this.areaNames )
-    },
-    async initCategoryNames() {
-      await StateHelper.load('category')
-      this.categoryNames = StateHelper.getOptionsFromState('category', false, false, 
-        category => !CATEGORY.POT_AVAILABLE.includes(category.categoryType)
-      )
-    },
     async save() {
       const notifyTemplateId = Util.hasValue(this.form.notifyTemplateId)? this.form.notifyTemplateId: -1
       const aNotifyState = (this.form.notifyTemplateKey != null)?this.form.notifyTemplateKey:0
       const entity = {
         notifyTemplateId: notifyTemplateId,
         notifyTemplateKey: aNotifyState,
-        notifyMedium: this.form.notifyMedium,
-        notifyTo: this.form.notifyTo,
+        notifyMedium: this.form.notifyMedium ? this.form.notifyMedium -1: '' ,
+        notifyTo: this.form.notifyTo? this.notifyTo:'',
         subject: this.form.subject,
         mailFrom: this.form.mailFrom,
         template: this.form.template,
