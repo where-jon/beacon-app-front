1891858d6a29913982fc34c615245fc97845d196
p_jung <p_jung@where123.jp>
2019-03-05 11:49:58 +0900

    お知らせ画面構成

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -8,10 +8,19 @@
     <b-button :variant="theme" class="btn-lg btn-block" type="submit">
       <i class="fas fa-sign-in-alt" />&nbsp;&nbsp;{{ $i18n.tnl('label.login') }}
     </b-button>
+    <div v-if="isNews">
+      <m-list :params="params" :list="newsList" />
+    </div>
   </form>
 </template>
 
 <script>
+import mList from '../components/page/list.vue'
+import * as Util from '../sub/util/Util'
+import { mapState } from 'vuex'
+import breadcrumb from '../components/layout/breadcrumb.vue'
+import * as ViewHelper from '../sub/helper/ViewHelper'
+import * as StateHelper from '../sub/helper/StateHelper'
 import { mapMutations } from 'vuex'
 import * as AuthHelper from '../sub/helper/AuthHelper'
 import { getButtonTheme } from '../sub/helper/ThemeHelper'
@@ -19,12 +28,20 @@ import { APP,DISP } from '../sub/constant/config'
 import commonmixinVue from '../components/mixin/commonmixin.vue'
 
 export default {
+  components: {
+    mList,
+    breadcrumb
+  },
   mixins: [commonmixinVue],
   data() {
     return {
+      isNews:true,
+      newsList: [],
+      appServicePath: '/signin',
       userId: '',
       password: '',
       message: '',
+      tenantAction: false,
       btnClasses: {
         'btn-primary': DISP.THEME === 'primary' || DISP.THEME === 'default',
         'btn-secondary': DISP.THEME === 'secondary',
@@ -33,15 +50,51 @@ export default {
         'btn-warning': DISP.THEME === 'warning',
         'btn-danger': DISP.THEME === 'danger',
         'btn-dark': DISP.THEME === 'dark',
-      }
+      },
+      params: {
+        name: 'news',
+        fields: ViewHelper.addLabelByKey(this.$i18n, [
+          {key: 'newsDate', sortable: true },
+          {key: 'content', sortable: true }
+        ]),
+      },
+      initTotalRows: this.$store.state.app_service.news.length,
+      items: ViewHelper.createBreadCrumbItems('test', 'osirase'),
     }
   },
   computed: {
+    ...mapState('app_service', [
+      'news',
+      'forceFetchNews',
+    ]),
     theme() {
       return getButtonTheme()
     },
   },
   methods: {
+    async fetchData(payload) {
+      try {
+        this.showProgress()
+        await StateHelper.load('news')
+        //await StateHelper.load('news', this.forceFetchNews)
+        //StateHelper.setForceFetch('news', false)
+        console.log('this.isRegistable::' + this.isRegistable)
+        this.newsList = this.news.map((val) => ({
+          ...val,
+          //newsId: val.newsId,
+          newsDate: Util.formatDate(val.newsDate),
+          content: val.content,
+        })) // omit images to avoid being filtering target
+        this.newsList.length > 0 ?this.isNews = true: this.isNews = false
+        if (payload && payload.done) {
+          payload.done()
+        }
+      }
+      catch(e) {
+        console.error(e)
+      }
+      this.hideProgress()
+    },
     ...mapMutations('setting', [
       'replaceSetting', 
     ]),
