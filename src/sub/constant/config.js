d13351df544ed668d79ce9f01d2eca56feda150d
imai <t_imai@where123.jp>
2019-03-11 17:43:08 +0900

    設定を戻す

--- src/sub/constant/config.js
+++ src/sub/constant/config.js
@@ -46,7 +46,7 @@ export const APP = { // 機能面に関する設定
   RSSI_MIN: -67, // RSSI下限値
   MOVING_AVERAGE: 5, // 5回分移動平均
   USE_MULTI_POSITIONING: false, // ３点測位を使う
-  EXSERVER: true, // EXServerを使う
+  EXSERVER: false, // EXServerを使う
   
   // 機能
   USE_THERMOPILE: true, // サーモパイルセンサーの使用
