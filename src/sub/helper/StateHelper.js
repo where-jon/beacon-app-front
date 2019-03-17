90210ca3282dd39e123f82bc13fc2a326c40f9a0
tsukada <t_tsukada@where123.jp>
2019-03-17 21:25:48 +0900

    進入禁止アラートでJavascriptエラーになる暫定対応

--- src/sub/helper/StateHelper.js
+++ src/sub/helper/StateHelper.js
@@ -352,16 +352,16 @@ const appStateConf = {
   prohibits: {
     path: '/core/zone/prohibit',
     beforeCommit: (arr) => {
-      let result = arr.map((val) => ({
+      let result = arr.map((val) => (val? { // TODO: valがundefinedになる
         ...val,
-        zoneId:val.zoneId,
+        zoneId: val.zoneId,
         zoneName:val.zoneName,
         x: val.x,
         y: val.y,
         w: val.w,
         h: val.h,
         areaId: val.areaId,
-      }))
+      }: null))
       return result
     }
   },
