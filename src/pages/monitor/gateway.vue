f308cef3c55cd630b01e0fe70cd8bf7e8335b626
imai <t_imai@where123.jp>
2019-03-22 16:38:52 +0900

    GWのcsvダウンロードを修正 for 1.0.1

--- src/pages/monitor/gateway.vue
+++ src/pages/monitor/gateway.vue
@@ -53,7 +53,7 @@ export default {
       csvHeaders: {
         'num': 'num',
         'deviceid': 'deviceid',
-        'timestamp': 'timestamp',
+        'updated': 'updated',
         'state': 'state',
       },
 
