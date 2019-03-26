f61c15b860e3dc2585ffb29630db808e18827b2f
m_nishiyama <m_nishiyama@where123.jp>
2019-03-26 21:45:22 +0900

    位置表示(一覧)のTXレコードのフィルタリングを修正

--- src/components/mixin/showmapmixin.vue
+++ src/components/mixin/showmapmixin.vue
@@ -387,16 +387,18 @@ export default {
     getShowTxPositions(positions, allShow = false){
       const now = !DEV.USE_MOCK_EXC ? new Date().getTime(): mock.positions_conf.start + this.count++ * mock.positions_conf.interval
       const correctPositions = APP.USE_POSITION_HISTORY? this.positionHistores: PositionHelper.correctPosId(this.orgPositions, now)
-      return _(positions).filter((pos) => pos.tx && Util.bitON(pos.tx.disp, TX.DISP.POS)).map((pos) => {
-        let cPos = _.find(correctPositions, (cPos) => pos.btx_id == cPos.btx_id)
-        if (cPos || allShow) {
-          return {
-            ...pos,
-            transparent: !cPos? true: cPos.transparent? cPos.transparent: PositionHelper.isTransparent(cPos.timestamp, now)
+      return _(positions)
+        .filter((pos) => allShow || (pos.tx && Util.bitON(pos.tx.disp, TX.DISP.POS)))
+        .map((pos) => {
+          let cPos = _.find(correctPositions, (cPos) => pos.btx_id == cPos.btx_id)
+          if (cPos || allShow) {
+            return {
+              ...pos,
+              transparent: !cPos? true: cPos.transparent? cPos.transparent: PositionHelper.isTransparent(cPos.timestamp, now)
+            }
           }
-        }
-        return null
-      }).compact().value()
+          return null
+        }).compact().value()
     },
     setPositionStyle(positions){
       return _.map(positions, pos => {
