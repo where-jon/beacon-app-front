17aeb03381cbaa49d7576a6ca3dd96a19f44b02c
t_fukushi <t_fukushi@where123.jp>
2019-04-03 16:29:56 +0900

    EXBeacon一括登録(新規登録/更新)時"sensor"の入力値が空欄又は不正な値の場合"...

--- src/pages/master/exb/bulkedit.vue
+++ src/pages/master/exb/bulkedit.vue
@@ -95,9 +95,12 @@ export default {
       const exbSensorList = []
       sensorNameList.forEach((sensorName) => {
         const sensor = this.sensorOptionsExb.find((option) => option.text == sensorName)
-        if(sensor && sensor.value != null){
-          exbSensorList.push({exbSensorPK: {sensorId: sensor.value}, sensorName: sensorName})
-        }
+        exbSensorList.push({
+          exbSensorPK: {
+            sensorId: sensor && sensor.value != null? sensor.value: dummyKey--
+          },
+          sensorName: sensorName
+        })
       })
       entity.exbSensorList = exbSensorList
       return dummyKey
