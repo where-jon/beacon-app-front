e4e36653ec43e5eb9ed58a3fe08a5bbaccfd5f9c
t_fukushi <t_fukushi@where123.jp>
2019-04-23 11:59:59 +0900

    温湿度画面でツールチップが文字がはみ出て表示される

---
+++ src/components/parts/toolTip.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-container>
+    <span v-if="toolTipShow" id="toolTipComponent" :style="toolTipStyle">
+      <div v-if="typeof toolTipLabel == 'string'">
+        <span>
+          {{ toolTipLabel }}
+        </span>
+      </div>
+      <div v-else>
+        <div v-for="(item, index) in toolTipLabel" :key="index">
+          <span v-if="showBlankRow || (item != null && item.length != 0)">
+            {{ item }}
+          </span>
+        </div>
+      </div>
+    </span>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    toolTipShow: {
+      type: Boolean,
+      default: false,
+    },
+    toolTipLabel: {
+      type: [Array, String],
+      default: '',
+    },
+    toolTipStyle: {
+      type: Object,
+      default: () => {},
+    },
+    showBlankRow: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style>
+#toolTipComponent {
+  position: absolute;
+  z-index: 999;
+
+  border-style: solid;
+  border-width: 1px;
+  line-height: 0px;
+  display: block;
+  padding: 8px;
+  left: 0px;
+  top: 0px;
+}
+</style>
