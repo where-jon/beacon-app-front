02855a7a8ba6b15b4613c8db2f5505eceb3bdc9f
t_matsumura <t_matsumura@where123.jp>
2019-04-25 11:08:25 +0900

    Lintのwarning対応

--- src/components/layout/nav.vue
+++ src/components/layout/nav.vue
@@ -1,8 +1,7 @@
 <template>
   <b-navbar :class="topNavBarClasses" toggleable="md" type="dark">
-    
-    <b-modal size="lg" id="helpModal" :title="$t('label.help')" ok-only>
-      <help :from-page="fromPageUrl"/>
+    <b-modal id="helpModal" size="lg" :title="$t('label.help')" ok-only>
+      <help :from-page="fromPageUrl" />
     </b-modal>
     <!-- Responsive menu -->
     <b-navbar-toggle v-show="!isLoginPage && getShowNav()" target="nav_collapse" />  
