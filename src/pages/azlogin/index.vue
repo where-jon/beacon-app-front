84ab78c7e41512fb6faa306cd4bc88c788250fa4
m_nishiyama <m_nishiyama@where123.jp>
2019-11-13 14:52:42 +0900

    モバイル版TeamsAppのコンテキストJSONをbase64化して送信

--- src/pages/azlogin/index.vue
+++ src/pages/azlogin/index.vue
@@ -101,9 +101,8 @@ export default {
     mobileLogin() {
       AADHelper.getContextForMobile((context) => {
         context.authTimestamp = new Date().getTime()
-        // const encryptedContext = CryptoJS.AES(JSON.stringify(context), MSTEAMS_APP.AES_KEY)
-        // AuthHelper.auth('MOBILE:' + encryptedContext, 'password',
-        AuthHelper.auth('MOBILE:' + context, 'password',
+        const base64 = btoa(unescape(encodeURIComponent(JSON.stringify(context))))
+        AuthHelper.auth('MOBILE:' + base64, 'password',
           ()=>{
             this.$router.push(APP.MENU.TOP_PAGE)
           },
