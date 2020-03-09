fca797494d73b9067509b5e9e60ada470e257539
Masatoshi Nishide <m_nishide@where123.jp>
2020-03-09 13:36:18 +0900

    APP以外の設定も比較対象へ追加、結果をファイル出力

--- src/test/sub/locales/Compare-config.test.js
+++ src/test/sub/locales/Compare-config.test.js
@@ -1,39 +1,51 @@
 import test from 'ava'
+import fs from 'fs'
 import ja from '../../../sub/locales/ja.json'
-import {APP} from '../../../sub/constant/config.js'
+import {APP, APP_SERVICE, EXCLOUD, DISP} from '../../../sub/constant/config.js'
+const resuletFile = 'resultCompareConfig.txt'
 
+// オブジェクトの階層を「.」で結合
 const keyJoin = (obj, keys = [], result = []) => {
-  Object.keys(obj).forEach(key => {
-    // 数値のみのキーが含まれる場合、上位に説明ありのため、除外
-    if(!isNaN(key)){
-      return
-    }
-    keys.push(key)
-    if(obj[key] && typeof obj[key] == 'object'){
-      keyJoin(obj[key], keys, result)
+  if(obj) {
+    Object.keys(obj).forEach(key => {
+      // キーが数値のみの場合、上位のキーに説明があるため、除外
+      if(!isNaN(key)){
+        return
+      }
+      keys.push(key)
+      if(obj[key] && typeof obj[key] == 'object'){
+        keyJoin(obj[key], keys, result)
+        keys.pop()
+        return
+      }
+      result.push( keys.join('.'))
       keys.pop()
-      return
-    }
-    result.push( keys.join('.'))
-    keys.pop()
-  })
+    })
+  }
   return result
 }
 
-test('比較テスト', async t => {
-  console.log('\n> ----- compare start -----\n')
-  const conf = keyJoin(APP)
-  const confJa = keyJoin(ja.config.APP)
+// 比較
+const compare = (conf,confJa,confName) =>{
   let confNotExitProp = []
+  fs.appendFileSync(resuletFile, `----- ${confName} -----\n`)
   conf.concat(confJa).forEach(item => {
     if (conf.includes(item) && !confJa.includes(item)) {
-      console.log(`ja.config.APPに存在しない : ${item}`)
+      fs.appendFileSync(resuletFile, `${item}\n`)
       confNotExitProp.push(item)
     }
   })
+  console.log(`ja.config.${confName} not exists items：` + confNotExitProp.length + '\n')
+}
 
-  console.log('\n> ja.configに不足している設定数：' + confNotExitProp.length + '\n')
-  console.log('\n> ----- compare success -----\n')
-  console.log('\n')
+// 実行
+test('compareConfig', async t => {
+  console.log('\n> ----- compare start -----\n')
+  fs.writeFileSync(resuletFile, 'compareConfig\n')
+  compare(keyJoin(APP),keyJoin(ja.config.APP) , 'APP')
+  compare(keyJoin(APP_SERVICE),keyJoin(ja.config.APP_SERVICE) ,'APP_SERVICE')
+  compare(keyJoin(EXCLOUD),keyJoin(ja.config.EXCLOUD) ,'EXCLOUD')
+  compare(keyJoin(DISP),keyJoin(ja.config.DISP) ,'DISP')
+  console.log('\n> ----- compare end -----\n')
   t.true(true)
 })
