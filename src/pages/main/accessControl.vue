348b97e9bfe260ca64058b72cfbfeb587919c929
imai <t_imai@where123.jp>
2020-04-15 14:10:48 +0900

    入退室管理　出社ステータス実装

--- src/pages/main/accessControl.vue
+++ src/pages/main/accessControl.vue
@@ -37,15 +37,19 @@
             <span v-t="'label.date'" class="d-flex align-items-center" />
             <date-picker v-model="form.date" :clearable="false" type="date" class="ml-2 inputdatefrom" required />
 
-            <b-button v-t="'label.display'" type="submit" :variant="theme" @click="display(false)" class="ml-2" />
-            <b-button v-t="'label.download'" type="submit" :variant="theme" @click="display(true)" class="ml-2" />
+            <b-button v-t="'label.display'" type="submit" :variant="theme" @click="display" class="ml-2" />
+            <b-button v-t="'label.download'" type="submit" :variant="theme" @click="download" class="ml-2" />
           </b-form-row>
         </b-form-group>
       </b-form>
 
-      <slot />
-      <b-row class="mt-3" />
-      <b-table :items="viewList" :fields="getField()" :current-page="currentPage" :per-page="perPage" :sort-by.sync="sortBy" :sort-compare="defaultSortCompare" stacked="md" striped hover outlined />
+      <b-row class="mt-2" v-if="allDayWorkPer">
+        {{$i18n.tnl("label.allDayWorkPer")}} {{allDayWorkPer}} {{$i18n.tnl("label.halfDayWorkPer")}} {{halfDayWorkPer}}
+      </b-row>
+
+      <b-row class="mt-3">
+        <b-table :items="viewList" :fields="getField()" :current-page="currentPage" :per-page="perPage" :sort-by.sync="sortBy" :sort-compare="defaultSortCompare" stacked="md" striped hover outlined />
+      </b-row>
       <b-row>
         <b-col md="6" class="mt-1 mb-3">
           <b-pagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" class="my-0" />
@@ -63,10 +67,12 @@ import 'element-ui/lib/theme-chalk/index.css'
 import { APP, DEV } from '../../sub/constant/config'
 import { CATEGORY } from '../../sub/constant/Constants'
 import * as DateUtil from '../../sub/util/DateUtil'
+import * as BrowserUtil from '../../sub/util/BrowserUtil'
 import * as Util from '../../sub/util/Util'
 import * as ViewHelper from '../../sub/helper/ui/ViewHelper'
 import * as MasterHelper from '../../sub/helper/domain/MasterHelper'
 import * as HttpHelper from '../../sub/helper/base/HttpHelper'
+import * as CharSetHelper from '../../sub/helper/base/CharSetHelper'
 import breadcrumb from '../../components/layout/breadcrumb.vue'
 import commonmixin from '../../components/mixin/commonmixin.vue'
 import alert from '../../components/parts/alert.vue'
@@ -89,13 +95,12 @@ export default {
       },
       message: '',
       viewList: [],
-      //name: '',
       currentPage: 1,
       perPage: 20,
       sortBy: 'name',
       totalRows: 0,
-      potMap: null,
-      exbMap: null
+      allDayWorkPer: null,
+      halfDayWorkPer: null
     }
   },
   computed: {
@@ -122,14 +127,15 @@ export default {
         {key: 'exitTime', sortable: false, label: this.$i18n.tnl('label.exitTime') },
         {key: 'lastDetected', sortable: false, label: this.$i18n.tnl('label.lastDetected') },
         {key: 'stayTime', sortable: false, label: this.$i18n.tnl('label.stayTime') },
+        {key: 'status', sortable: false, label: this.$i18n.tnl('label.status') },
       ]
     },
-    async display(isDownload) {
+    async display() {
       Util.debug('form', this.form)
 
-      this.replace({showAlert: false})
-      this.showProgress()
       try {
+        this.replace({showAlert: false})
+        this.showProgress()
         if ( !Util.hasValue(this.form.date) ) {
           this.message = this.$i18n.tnl('message.pleaseEnterSearchCriteria')
           this.replace({showAlert: true})
@@ -146,6 +152,8 @@ export default {
           return
         }
 
+        let allCount = 0
+        let halfCount = 0
         this.viewList = res.map( e => {
           const pot = this.potIdMap[e.potId]
           console.log('pot', pot)
@@ -153,16 +161,34 @@ export default {
           const location = this.locationIdMap[e.locationId]
           console.log('location', location)
           const zoneName = location ? location.zoneList.length>0 && location.zoneList[0].zoneName : null
+
+          // 勤務時間判定 TODO:仮実装
+          let status = ''
+          if((e.outDt - e.inDt)>=1000*60*60*9){
+            status = this.$i18n.tnl("label.allDayWork")
+            allCount++
+          }else if((e.outDt - e.inDt)>=1000*60*60*4){
+            status = this.$i18n.tnl("label.halfDayWork")
+            halfCount++
+          }else{
+            status = this.$i18n.tnl("label.temporaryTimeWork")
+          }
           return {
             name: e.potName, 
             groupName: group ? group.groupName : null,
             entranceTime: DateUtil.formatDate(e.inDt),
             exitTime: DateUtil.formatDate(e.outDt),
             lastDetected: zoneName,
-            stayTime: DateUtil.toHHmm( (e.outDt - e.inDt)/1000)
+            stayTime: DateUtil.toHHmm( (e.outDt - e.inDt)/1000),
+            status
           }
         })
         Util.debug("viewList", this.viewList)
+
+        if(this.viewList.length > 0){
+          this.allDayWorkPer = Math.floor(allCount / this.viewList.length * 100) + '%'
+          this.halfDayWorkPer = Math.floor(halfCount / this.viewList.length * 100) + '%'
+        }
 
         this.totalRows = this.viewList.length
       }
@@ -178,11 +204,17 @@ export default {
       const potId = this.form.pot ? this.form.pot.value : 0
       const groupId = this.form.group ? this.form.group.value : 0
       const url = `/office/accessControl/list/${date}/${potId}/${groupId}`
-      Util.debug('url', url)
       const data = await HttpHelper.getAppService(url)
       Util.debug('data', data)
       return data
     },
+    async download(){
+      let csv = _.map(this.getField(), e => e.label).join(',') + '\n' // ヘッダー
+      _.forEach(this.viewList, v => {
+        csv += `${v.name},${v.groupName ? v.groupName : ''},${v.entranceTime},${v.exitTime},${v.lastDetected},${v.stayTime},${v.status}\n`
+      })
+      BrowserUtil.fileDL('accessControl.csv', csv, CharSetHelper.getCharSet(this.$store.state.loginId))
+    }
   }
 }
 </script>
