05f03713a387b2f143c7e6e9bd21a5fb7c5b1377
imai <t_imai@where123.jp>
2020-04-15 17:58:53 +0900

    出力フォーマット修正 #3094

--- src/pages/sum/meetingGraph.vue
+++ src/pages/sum/meetingGraph.vue
@@ -142,7 +142,7 @@ export default {
           sum[time].forEach(pos => {
             const cdName = this.getCdName(pos)
             const name = cdName.name ? (cdName.name + ",") : ""
-            csv += DateUtil.formatDateWithTimeZone(time) + "," + cdName.cd + "," + name + pos.cnt + "\n"
+            csv += DateUtil.formatDateWithTimeZone(time, 'YYYY/MM/DD HH:mm') + "," + cdName.cd + "," + name + pos.cnt + "\n"
           })
         }
       }
