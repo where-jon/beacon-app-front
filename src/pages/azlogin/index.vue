2cad4b4f5854d88901abf6e176a3cfe4d6bd0d49
tsukada <t_tsukada@where123.jp>
2020-05-21 20:02:54 +0900

    #2810 デバッグ追加

--- src/pages/azlogin/index.vue
+++ src/pages/azlogin/index.vue
@@ -145,8 +145,9 @@ export default {
       // microsoftTeams.initialize()
       LocalStorageHelper.setLocalStorage('tenantName', this.tenantName)
       // const redirectUrl = encodeURIComponent(APP.AUTH.REDIRECT_URL.split('end').join('adminend'))
-      const redirectUrl = encodeURIComponent(APP.AUTH.REDIRECT_URL.split('end/').join('')) + '?admin_consent=True'
+      const redirectUrl = encodeURIComponent(APP.AUTH.REDIRECT_URL.split('end/').join('')) + '%3Fadmin_consent=True'
       const adminConsentUrl = 'https://login.microsoftonline.com/common/adminconsent?client_id=' + APP.AUTH.APP_ID + '&redirect_uri=' + redirectUrl
+      alert(adminConsentUrl)
       location.href = adminConsentUrl
       // microsoftTeams.authentication.authenticate({
       //   url: adminConsentUrl,
